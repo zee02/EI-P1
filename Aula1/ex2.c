0c45cfb572691eb0f0aa30ef00063d71fb311b66
José Fernandes <zefrancisco2002@gmail.com>
2021-10-06 11:50:00 +0100

    Ex 1,2,3

--- Aula1/ex2.c
+++ Aula1/ex2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world!");
+    printf("Exemplo de tabulação\t");
     return 0;
 }
---
+++ Aula1/ex3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Olá! Insira um número para ver o que acontece!: ");
+    scanf("%d", &num);
+    printf("O seu número é: %d \n", num);
+    return 0;
+}
