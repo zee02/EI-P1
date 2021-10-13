4c0fbe158f47ace950d5fe8101772d0d6179acb0
José Fernandes <zefrancisco2002@gmail.com>
2021-10-13 12:02:52 +0100

    3

--- Códigos 1ºSemestre/ficha2_3.c
+++ Códigos 1ºSemestre/ficha2_3.c
@@ -12,10 +12,10 @@ int main() {
     printf("Apresente o terceiro número inteiro: \t");
     scanf("%d", &num3);
 
+    if(num1 != num2 && num1 != num3 && num2 != num3) {
+        printf("Todos os números introduzidos são diferentes\t");
+    }
     if(num1 == num2 || num1 == num3 || num2 == num3) {
         printf("Foram introduzidos números iguais\t");
-    }
-    else {
-        printf("Foram introduzidos números diferentes\t");
     }
 }
