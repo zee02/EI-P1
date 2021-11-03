d0d9a09a62256b6977e2a717de56e16a62ee3867
José Fernandes <zefrancisco2002@gmail.com>
2021-11-03 12:44:45 +0000

    ...

--- Códigos 1ºSemestre/ficha4_1.c
+++ Códigos 1ºSemestre/ficha4_1.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define PI 3, 14, 16
+int leNumero();
+int areaRetangulo(int, int);
+int leDadosRetangulo(int *x);
+void leDadosRetangulo2(int *x, int *y);
 int main()
 {
-    int lado1, lado2, raio,area;
+    int lado1, lado2, raio, area;
     char op;
-    
-
-    lado1 = leNumero();
 
     do
     {
-        printf("Indique o comprimento do lado 2: ");
-        scanf("%d", &lado2);
-
-    } while (lado2 <= 0);
-
-    area = lado1 + lado2;
-
-    printf("Area = $d", area);
+        op = menu();
+    } while (op != 'F');
     return 0;
 }
 
+char menu()
+{
+    char opcao;
+    printf("Triangulo (No): ***Circulo (No): **OPCOES(T)riangulo(R)etangulo(Q)uadrado(C)irculo(F)im Selecione opcao: _");
+}
 int leNumero()
 {
     int num;
@@ -33,13 +34,13 @@ int leNumero()
     return num;
 }
 
-float areaCirculo(){
+float areaCirculo(int *pi, *r, *r)
+{
     float area;
-    area = pi*r*r;
+    area = pi * r * r;
     return area;
-
 }
 
-int areaRetangulo() {
-    
+int areaRetangulo()
+{
 }
