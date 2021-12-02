56bceb436e49c6e5cc74b1d7c9c59292005f233c
José Fernandes <zefrancisco2002@gmail.com>
2021-12-02 15:20:32 +0000

    Aula extra changes ex 6

--- Códigos 1ºSemestre/ficha6_1.c
+++ Códigos 1ºSemestre/ficha6_1.c
@@ -1,69 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #define MAX_STRING 50
 #define MAX_ESTUDANTES 100
 typedef struct
 {
     int numero;
-    char nome[255];
+    char nome[MAX_STRING];
     int nota;
 } tipoEstudante;
-
 int lerInteiro(int minimo, int maximo);
 void limpaBufferStdin(void);
-int lerQuantidadeEstudantes();
+void lerNotas(tipoEstudante v[MAX_ESTUDANTES], int limite);
+void mostrarDados(tipoEstudante v[MAX_ESTUDANTES], int limite);
 tipoEstudante lerDadosEstudante();
-void mostrarDados();
+int lerQuantidadeEstudantes();
+int lerInteiro(int minimo, int maximo);
+void limpaBufferStdin(void);
+
 int main()
 {
     int numEstudantes;
-
     tipoEstudante turma[MAX_ESTUDANTES];
     numEstudantes = lerQuantidadeEstudantes();
     for (int i = 0; i < numEstudantes; i++)
     {
-        turma[0] = lerDadosEstudante();
+        turma[i] = lerDadosEstudante();
     }
-    for (int i = 0; i < numEstudantes; i++)
-    {
-        printf("Numero: \n%d  Nome: %s", turma[0].numero, turma[0].nome);
-    }
-
+    mostrarDados(turma, numEstudantes);
+    lerNotas(turma, numEstudantes);
     mostrarDados(turma, numEstudantes);
     return 0;
 }
-
 void lerNotas(tipoEstudante v[MAX_ESTUDANTES], int limite)
 {
-
-    for (int i = 0; i < limite; i++)
+    int i;
+    for (i = 0; i < limite; i++)
     {
-        printf("Estudante Numero: %d - %s", v[i].numero, v[i].nome);
-        v[i].nota = lerInteiro(0,20);
+        printf("Estudante: %d  - %s", v[i].numero, v[i].nome);
+        v[i].nota = lerInteiro(0, 20);
     }
 }
-
 void mostrarDados(tipoEstudante v[MAX_ESTUDANTES], int limite)
 {
-    for (int i = 0; i < limite; i++)
+    int i;
+    for (i = 0; i < limite; i++)
     {
-        printf("Numero: \n%d  \nNome: %s", v[i].numero, v[i].nome);
-
-        if(v[i].nota!=-1)
-        printf("A nota do estudante foi: %d", v[i].nota);
+        printf("Numero: %d   Nome: %s", v[i].numero, v[i].nome);
+        if (v[i].nota != -1)
+            printf("Nota: %d", v[i].nota);
     }
 }
-
 tipoEstudante lerDadosEstudante()
 {
     tipoEstudante e;
     int num;
-    printf("Indique numero: ");
-    num = lerInteiro(1, 9999);
-    printf("Indique nome: ");
+    printf("Indique numero ");
+    e.numero = lerInteiro(1, 9999);
+    printf("Indique nome ");
     fgets(e.nome, MAX_STRING, stdin);
+    e.nota = -1;
     return e;
+}
+
+int lerQuantidadeEstudantes()
+{
+    int quant;
+    printf("Quantidade de estudantes");
+    quant = lerInteiro(1, MAX_ESTUDANTES);
+    return quant;
 }
 
 int lerInteiro(int minimo, int maximo)
@@ -96,14 +100,4 @@ void limpaBufferStdin(void)
     {
         chr = getchar();
     } while (chr != '\n' && chr != EOF);
-}
-
-int lerQuantidadeEstudantes()
-{
-    int quant;
-
-    printf("Indique quantidade de estudantes: ");
-    quant = lerInteiro(1, MAX_ESTUDANTES);
-
-    return quant;
 }
